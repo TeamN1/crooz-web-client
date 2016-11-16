69de860781ff1554f99aae6e6620f69d0cc054b2
Brian Mbuba <yourBrian@users.noreply.github.com>
2016-11-16 13:01:42 +1300

    Update crooz.js

--- public/js/crooz.js
+++ public/js/crooz.js
@@ -5,11 +5,25 @@ function main() {
     socket = io();
     mapper = Mapper.init(document.getElementById('mapCard'));
     socket.on('connected', function (users) {
-        socket.emit('subscribe', users[0]);
+        socket.emit('subscribe', users[users.length-1]);
     });
     socket.on('newPacket', function (packet) {
         console.log(packet);
         mapper.addPackets([packet]);
+        console.log(mapper._packets);
+        mapper.render();
+        var car = mapper.car;
+        document.getElementById('songCard').innerText = car.song;
+        document.getElementById('moodCard').innerText = Object.keys(car.mood).reduce(
+            function(a, b) {
+                return car.mood[a] > car.mood[b] ? a : b
+            }
+        );
+    });
+    
+    socket.on('newPackets', function (packets) {
+        console.log(packets);
+        mapper.addPackets(packets);
         console.log(mapper._packets);
         mapper.render();
         var car = mapper.car;
