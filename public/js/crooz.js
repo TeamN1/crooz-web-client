a548e86c0b628b870b80c8eb95eff12a335f5d35
Brian Mbuba <yourBrian@users.noreply.github.com>
2016-11-17 08:31:10 +1300

    Update crooz.js

--- public/js/crooz.js
+++ public/js/crooz.js
@@ -6,35 +6,27 @@ function main() {
     mapper = Mapper.init(document.getElementById('mapCard'));
     socket.on('connected', function (users) {
         socket.emit('subscribe', users[0]);
-        console.log(users);
+//         console.log(users);
     });
     socket.on('newPacket', function (packet) {
-        if(!packet) {return}
-        console.log(packet);
-        mapper.addPackets([packet]);
-        console.log(mapper._packets);
-        mapper.render();
-        var car = mapper.car;
-        document.getElementById('songCard').innerText = car.song;
-        document.getElementById('moodCard').innerText = Object.keys(car.mood).reduce(
-            function(a, b) {
-                return car.mood[a] > car.mood[b] ? a : b
-            }
-        );
+        handlePackets([packet]);
     });
     
     socket.on('newPackets', function (packets) {
+        handlePackets(packets);
+    });
+    function handlePackets(packets) {
         if(!packets.length) {return}
-        console.log(packets);
         mapper.addPackets(packets);
-        console.log(mapper._packets);
+//         console.log(mapper._packets);
         mapper.render();
         var car = mapper.car;
+        document.getElementById('speedCard').innerText = car.speed*3.6 + " km/h";
         document.getElementById('songCard').innerText = car.song;
         document.getElementById('moodCard').innerText = Object.keys(car.mood).reduce(
             function(a, b) {
                 return car.mood[a] > car.mood[b] ? a : b
             }
         );
-    });
+    }
 }
